c5060e0a049718382beefe338bc8969b00171598
yloose <yanloose@gmail.com>
2024-06-22 11:05:50 +0200

    Add run script

--- Containerfile
+++ Containerfile
@@ -1,9 +1,16 @@
 FROM docker.io/wolframresearch/wolframengine
 
 USER root
+
+RUN useradd -m wljs
+
+RUN apt-get update && apt-get install -y git
+
 RUN mkdir -p /wljs
 WORKDIR /wljs
-RUN apt-get update && apt-get install -y git
 RUN git clone https://github.com/JerryI/wolfram-js-frontend .
 
-CMD wolframscript -f Scripts/start.wls host 0.0.0.0 http 4000 ws 4001 ws2 4002 docs 4003 folder "/Workspace"
+COPY run.sh /run.sh
+RUN chmod +x /run.sh
+
+CMD /run.sh
---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PUID=${PUID:1000}
+PGID=${PGID:1000}
+
+groupmod -o -g "$PGID" wljs
+usermod -o -u "$PUID" wljs
+
+chown -R wljs:wljs /wljs
+chown -R wljs:wljs /home/wljs
+
+
+mkdir -p /home/wljs/.WolframEngine/Licensing
+cp /Licensing/mathpass /home/wljs/.WolframEngine/Licensing/mathpass
+chown -R wljs:wljs /home/wljs/.WolframEngine
+
+su wljs wolframscript -f /wljs/Scripts/start.wls host 0.0.0.0 http 4000 ws 4001 ws2 4002 docs 4003 folder "/Workspace"
