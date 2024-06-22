7bd77af4280157e105c5e50e2af3191cd2dfc684
yloose <yanloose@gmail.com>
2024-06-22 11:18:50 +0200

    Fix uid variable

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PUID=${PUID:1000}
-PGID=${PGID:1000}
+PUID=${PUID:-1000}
+PGID=${PGID:-1000}
 
 groupmod -o -g "$PGID" wljs
 usermod -o -u "$PUID" wljs
@@ -14,4 +14,4 @@ mkdir -p /home/wljs/.WolframEngine/Licensing
 cp /Licensing/mathpass /home/wljs/.WolframEngine/Licensing/mathpass
 chown -R wljs:wljs /home/wljs/.WolframEngine
 
-su wljs wolframscript -f /wljs/Scripts/start.wls host 0.0.0.0 http 4000 ws 4001 ws2 4002 docs 4003 folder "/Workspace"
+su wljs /usr/bin/wolframscript -f /wljs/Scripts/start.wls host 0.0.0.0 http 4000 ws 4001 ws2 4002 docs 4003 folder "/Workspace"
